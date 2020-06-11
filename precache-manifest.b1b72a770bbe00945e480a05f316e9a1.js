ef6aca6a12606be90270e8838f94cce46bbb15f1
khantmawhein <kokhant228abc@gmail.com>
2020-06-11 10:14:27 +0630

    Updates

---
+++ precache-manifest.b1b72a770bbe00945e480a05f316e9a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4812ba1d4402eba5d2e547dce74b0f2a",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "d18adbd687d7c4602954",
+    "url": "/todo/static/css/main.c6afa687.chunk.css"
+  },
+  {
+    "revision": "762ab9408711b9c7b5a7",
+    "url": "/todo/static/js/2.f84ca300.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.f84ca300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18adbd687d7c4602954",
+    "url": "/todo/static/js/main.76aab85c.chunk.js"
+  },
+  {
+    "revision": "77dd77f32cf080a89cb0",
+    "url": "/todo/static/js/runtime-main.c49290f7.js"
+  }
+]);
